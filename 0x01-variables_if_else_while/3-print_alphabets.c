257b3fac7554b96e87b42cf2b79447bbce861865
fredlaryea2000 <fredlaryea2000@gmail.com>
2023-02-20 11:17:44 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point
- * 
+ i 
  * Return: Always 0 (success)
  */
 int main(void)
@@ -15,16 +15,16 @@ int main(void)
 	d = 'A';
 	while
 		(c <= 'z') 
-		{
+	{
 		putchar(c);
 		c++;
-		}
+	}
 	while
 		(d <= 'Z') 
-		{
+	{
 			putchar(d);
 		        d++;
-		}
+	}
 	putchar('\n');
 	return (0);
 }
