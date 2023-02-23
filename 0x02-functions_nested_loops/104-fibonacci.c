7bfc18443b776e21382e96b5e820fc61302409aa
fredlaryea2000 <fredlaryea2000@gmail.com>
2023-02-22 16:41:14 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,54 +1,45 @@
 #include <stdio.h>
+/**
+ * main - first 98 fibonacci numbers.
+ * Return: return Nothing.
+ */
+int main(void)
+{
+int count;
+unsigned long i, j, k;
+unsigned long m, n, p, carry;
 
-
-	/**
-	 * main - fibonacci <3
-	 *
-	 * Purpose - no hardcode
-	 *
-	 * Return:  (Success)
-	 */
-
-
-	int main(void)
-	{
-		unsigned long int i;
-		unsigned long int bef = 1;
-		unsigned long int aft = 2;
-		unsigned long int l = 1000000000;
-		unsigned long int bef1;
-		unsigned long int bef2;
-		unsigned long int aft1;
-		unsigned long int aft2;
-
-
-		printf("%lu", bef);
-
-
-		for (i = 1; i < 91; i++)
-		{
-			printf(", %lu", aft);
-			aft += bef;
-			bef = aft - bef;
-		}
-
-
-		bef1 = (bef / l);
-		bef2 = (bef % l);
-		aft1 = (aft / l);
-		aft2 = (aft % l);
-
-
-		for (i = 92; i < 99; ++i)
-		{
-			printf(", %lu", aft1 + (aft2 / l));
-			printf("%lu", aft2 % l);
-			aft1 = aft1 + bef1;
-			bef1 = aft1 - bef1;
-			aft2 = aft2 + bef2;
-			bef2 = aft2 - bef2;
-		}
-		printf("\n");
-		return (0);
-	}
-
+count = 0;
+i = 0;
+j = 1;
+for (count = 1; count <= 91; count++)
+{
+k = i + j;
+i = j;
+j = k;
+printf("%lu, ", k);
+}
+m = i % 1000;
+i = i / 1000;
+n = j % 1000;
+j = j / 1000;
+while (count <= 98)
+{
+carry = (m + n) / 1000;
+p = (m + n) -carry * 1000;
+k = (i + j) +carry;
+m = n;
+n = p;
+i = j;
+j = k;
+if (p >= 100)
+printf("%lu%lu", k, p);
+else
+printf("%lu0%lu", k, p);
+if (count != 98)
+printf(", ");
+count++;
+}
+putchar('\n');
+return (0);
+}
