54308d57e811f28d8a8f0d327460bf3f006b7f6d
Fred Laryea <fredlaryea2000@gmail.com>
2023-06-19 12:00:04 -0700

    Updated

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Tolulope05/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+cp ../sh2.sh .
+. ./sh2.sh
